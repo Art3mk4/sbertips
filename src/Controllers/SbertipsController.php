1560bf228ac0c43883a18d573991ca394645b073
artem <art3mk4@gmail.com>
2024-03-12 11:34:46 +0600

    composer update

--- src/Controllers/SbertipsController.php
+++ src/Controllers/SbertipsController.php
@@ -9,6 +9,6 @@ class SbertipsController extends Controller
 {
 	public function index()
     {
-		return response('test index test', 200)->header('Content-Type', 'text/plain');
+		return response('test index test hoho', 200)->header('Content-Type', 'text/plain');
 	}
 }
