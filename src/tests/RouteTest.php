cdbf1527ad61dcecbed459ca4a9c279e3b2ae385
artem <art3mk4@gmail.com>
2024-03-13 13:12:55 +0600

    remove unused test

--- src/tests/RouteTest.php
+++ src/tests/RouteTest.php
@@ -17,15 +17,6 @@ class RouteTest extends TestCase
     /**
      * @return void
      */
-    public function test_sbertips_route(): void
-    {
-        $response = $this->get('/sbertips');
-        $response->assertStatus(200);
-    }
-
-    /**
-     * @return void
-     */
     public function test_qrcode_list_route(): void
     {
         $response = $this->get('/qrcode/list');
