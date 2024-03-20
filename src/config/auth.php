3707b9a891a5ed67fc77725e1164580df59f7868
artem <art3mk4@gmail.com>
2024-03-20 16:37:08 +0600

    fix config password

--- src/config/auth.php
+++ src/config/auth.php
@@ -4,5 +4,5 @@ return [
     'url'              => env('SBERTIPS_URL'),
     'merchantLogin'    => env('SBERTIPS_MERCHANT_LOGIN'),
     'merchantPassword' => env('SBERTIPS_MERCHANT_PASSWORD'),
-    'version'          => '1.1.5',
+    'version'          => '1.2.5',
 ];
