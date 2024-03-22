7ae9d7aec1ac5ff3ed9c8cf2af1e3279549157a9
artem <art3mk4@gmail.com>
2024-03-22 16:37:23 +0600

    change version

--- src/config/auth.php
+++ src/config/auth.php
@@ -4,7 +4,7 @@ return [
     'url'              => env('SBERTIPS_URL'),
     'merchantLogin'    => env('SBERTIPS_MERCHANT_LOGIN'),
     'merchantPassword' => env('SBERTIPS_MERCHANT_PASSWORD'),
-    'version'          => '1.2.5',
+    'version'          => '1.2.8',
     'auth'             => [
         'bearerToken'  => env('SBERTIPS_API_TOKEN')
     ]
